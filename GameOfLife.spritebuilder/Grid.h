32cc3254b47497cc1125a6c975583c877554651c
longma <longma@longmas-MacBook-Pro.local>
2015-01-26 22:38:48 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,15 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by longma on 1/26/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+
+@end
